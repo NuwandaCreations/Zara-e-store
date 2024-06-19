61ac44b4bf80f065bf4bb907aa18e30b9fa615d2
pablorojoabollado <nuwandacreations@gmail.com>
2024-06-19 22:16:20 +0200

    navigation implemented

--- app/src/main/java/com/example/zaraestore/MainActivity.kt
+++ app/src/main/java/com/example/zaraestore/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.zaraestore.ui.theme.ZaraEstoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +17,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            Navigation()
             ZaraEstoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZaraEstoreTheme {
-        Greeting("Android")
+    sealed class Screen(val route: String) {
+        data object HomeScreen : Screen("home")
+        data object ProductScreen : Screen("product")
+    }
+    @Composable
+    private fun Navigation() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = Screen.HomeScreen.route) {
+            composable(Screen.HomeScreen.route) {
+                Button(onClick = { navController.navigate(Screen.ProductScreen.route) }) {
+                    Text(text = "Go to Product Screen")
+                }
+            }
+            composable(Screen.ProductScreen.route) {
+                Text(text = "Product Screen")
+            }
+        }
     }
 }
